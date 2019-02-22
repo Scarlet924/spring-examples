d23e5c6f02a75fdf0f82bc7dcbcf1c39d22a4a69
SM5045659 <SM5045659@SYNPSZD4886.syntelorg.com>
2019-02-22 11:27:13 +0530

    commit1
    Change-Id: I710f996b31e06d76719c6d5d5ff8143a8959a85a

---
+++ Helloworld/src/com/GitDemo.java
@@ -0,0 +1,19 @@
+package com;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		
+		new GitDemo().print();
+	}
+
+	
+	void print(){
+		System.out.println("Printing.....");
+	}
+
+}
